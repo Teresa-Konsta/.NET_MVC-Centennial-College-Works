e1ec257bc8c2686b76b1f76d2879e5c2504afd26
Tereza Konstari <71859225+Teresa-Konsta@users.noreply.github.com>
2020-10-11 23:19:00 -0400

    Add files via upload

---
+++ recipe system/Assignment1/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Assignment1.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Assignment1.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userMg, SignInManager<IdentityUser> signInMg)
+        {
+            userManager = userMg;
+            signInManager = signInMg;
+        }
+
+        [AllowAnonymous]
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            { 
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken] //extra layer of security
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Name);
+                
+                if(user != null)
+                {
+                    if((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid name or/and password");
+            return View(loginModel);
+        }
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ recipe system/Assignment1/Models/IdentitySeedData.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "InitialPassword1$";
+        private const string adminRoleName = "Administrator";
+        private const string mngUser = "User";
+        private const string mngPassword = "InitialPassword2$";
+        private const string mngRoleName = "Manager";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            //role general
+            RoleManager<IdentityRole> roleManager = app.ApplicationServices.GetRequiredService<RoleManager<IdentityRole>>();
+            
+            //admin role
+            IdentityRole adminRole = await roleManager.FindByNameAsync(adminRoleName);
+            if(adminRole == null)
+            {
+                adminRole = new IdentityRole(adminRoleName);
+                await roleManager.CreateAsync(adminRole);
+            }
+
+            //manager role
+            IdentityRole mngRole = await roleManager.FindByNameAsync(mngRoleName);
+            if (mngRole == null)
+            {
+                mngRole = new IdentityRole(mngRoleName);
+                await roleManager.CreateAsync(mngRole);
+            }
+            
+            UserManager<IdentityUser> userManager = app.ApplicationServices.GetRequiredService<UserManager<IdentityUser>>();
+
+            //admin user
+            IdentityUser user = await userManager.FindByNameAsync(adminUser);
+
+            if(user == null)
+            {
+                user = new IdentityUser(adminUser);
+                await userManager.CreateAsync(user, adminPassword);
+                await userManager.AddToRoleAsync(user, adminRoleName);
+            }
+            else
+            {
+                if(!(await userManager.IsInRoleAsync(user, adminRoleName)))
+                {
+                    await userManager.AddToRoleAsync(user, adminRoleName);
+                }
+            }
+
+            //manager user
+            IdentityUser userMng = await userManager.FindByNameAsync(mngUser);
+
+            if (userMng == null)
+            {
+                userMng = new IdentityUser(mngUser);
+                await userManager.CreateAsync(userMng, mngPassword);
+                await userManager.AddToRoleAsync(userMng, mngRoleName);
+            }
+            else
+            {
+                if (!(await userManager.IsInRoleAsync(userMng, mngRoleName)))
+                {
+                    await userManager.AddToRoleAsync(userMng, mngRoleName);
+                }
+            }
+        }
+    }
+}
